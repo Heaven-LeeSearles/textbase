009449623efe26537c3a7e3f10d3ff2910a62577
Heaven-LeeSearles <heavenleesearles@gmail.com>
2017-07-21 14:15:38 -0400

    added

---
+++ TextAdventureGame.py
@@ -0,0 +1,15 @@
+print ("Welcome to a world without equality very similar to the one you probably already live in your objective is to make the best choice of yor options to raise your 'child' roleplaying as either a rich male or a single poor woman to the best of your abilities without them getting sick otherwise you lose")
+male = str (input ("Pick a person to roleplay male ceo or a female factory worker"))
+
+if male == "male":
+    print ("Youre a rich business man with endless opurtunities and an endless work load who has time to take care of kids just hire an overly qualified nanny to do the work for you you can afford it anyways ")
+
+care = str (input ("would you choose a nanny from care.com type care or for a highly certified nanny type certified nanny"))
+
+if care == "care":
+    print ("Thats way to low for you pick something else your rich dude: choose a nanny if you want one from care.com type care.com if you want highly certified nanny type certified nanny")
+
+certified = str (input ("would you choose a nanny from care.com type care or a highly certified nanny type certified" ))
+
+if certified== "certified":
+ print ("Great choice only the best for your child because if you could afford it why this nanny makes your kid the best that they could be and not only are they healty but they take over the company and end up on top youre the best parent despite parenting being such a difficult task to conquer for you prevailed and guess what you win")
